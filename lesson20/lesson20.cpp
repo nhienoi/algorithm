d6d5e425f2457ca31db40f25f18c4ac4fa66b1ef
nhienoi <nhienduyvu@gmail.com>
2021-06-21 23:11:56 +0700

    lesson20

---
+++ lesson20/lesson20.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+void type(int arr[], int n, int m);
+void exprt(int arr[], int n, int m);
+void type(int arr[], int n, int m) {
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			cout << "arr[" << n << "][" << m << "] = ";
+			cin >> arr[i][j];
+		}
+	}
+}
+void exprt(int arr[], int n, int m) {
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n;j++) {
+			cout << "arr[" << i << "][" << j << "] = " << arr[i][j];
+			cout << endl;
+		}
+	}
+}
+int main() {
+	int n, m;
+	cout << "n m";
+	cin >> n >> m;
+	int arr[n][m];
+	type(arr[], n, m);
+	exprt(arr[], n, m);
+	return 0;
+}
