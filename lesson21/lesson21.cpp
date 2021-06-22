2e21ac1d29b1d8f7692b4a6f7391aee891ecaad2
nhienoi <nhienduyvu@gmail.com>
2021-06-22 07:53:01 +0700

    oik

---
+++ lesson21/lesson21.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+bool isPrime(int n);
+
+bool isPrime(int n) {
+	bool ok = true;
+	if (n <= 1) {
+		ok = false;
+	}
+	for (int i = 2; i sqrt(n); i++) {
+		if (n % i == 0) {
+			ok = false;
+	}
+	return ok;
+}
+int main() {
+	int n;
+	cin >> n;
+	for (int i = 1; i < n; i++) {
+		if (isPrime(i)) {
+			cout << i << endl;
+		}
+	}
+	return 0;
+}
